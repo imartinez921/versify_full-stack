61f86eea49d6e5c08daedb98af24e60aeb31e143
Irene Martinez <imartinez921@gmail.com>
2023-06-29 17:11:33 -0400

    Fix routes formating

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-namespace :api, defaults: {format: :json} do
-  resource :session, only: [:create, :destroy]
-  resources :users, only: [:create]
-  resources :playlists, only: [:index, :show, :create, :update, :destroy]
-  resources :artists, only: [:index, :show]
-  resources :albums, only: [:index, :show]
-  resources :songs, only: [:index, :show]
-  resources :playlisteds, only: [:create, :destroy]
-end
+  namespace :api, defaults: {format: :json} do
+    resource :session, only: [:create, :destroy]
+    resources :users, only: [:create]
+    resources :playlists, only: [:index, :show, :create, :update, :destroy]
+    resources :artists, only: [:index, :show]
+    resources :albums, only: [:index, :show]
+    resources :songs, only: [:index, :show]
+    resources :playlisteds, only: [:create, :destroy]
+  end
   
   root 'static_pages#root'
 end
