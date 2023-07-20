4f3e04cb1b792c8a2d0c2d118b8cee731a591bea
Irene Martinez <imartinez921@gmail.com>
2023-07-20 18:11:15 -0400

    Original

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -7,35 +7,28 @@ import SplashContainer from "./splash/splash_container";
 import SignupFormContainer from "./session/signup_form_container";
 import LoginFormContainer from "./session/login_form_container";
 import MainContainer from "./main/main_container";
-import MobileWarning from "./errors/mobile_warning";
 // import EditPlaylistContainer from './playlists/edit_playlist_container'
 
-const App = () => {
-    return (process.env.REACT_APP_RAILS_REFACTOR === "true" ? (
+const App = () => (
+    <div>
         <Switch>
-            <Route exact path="/" component={MobileWarning} />
+            <Route exact path="/" component={SplashContainer} />
+            <LoggedoutRoute
+                exact
+                path="/signup"
+                component={SignupFormContainer}
+            />
+            <LoggedoutRoute
+                exact
+                path="/login"
+                component={LoginFormContainer}
+            />
+            <AuthRoute exact path="/home" component={MainContainer} />
+            <AuthRoute path="/playlist/:id" component={MainContainer} />
+            <AuthRoute path="/artist/:id" component={MainContainer} />
+            <AuthRoute path="/album/:id" component={MainContainer} />
         </Switch>
-    ) : (
-        <div>
-            <Switch>
-                <Route exact path="/" component={SplashContainer} />
-                <LoggedoutRoute
-                    exact
-                    path="/signup"
-                    component={SignupFormContainer}
-                />
-                <LoggedoutRoute
-                    exact
-                    path="/login"
-                    component={LoginFormContainer}
-                />
-                <AuthRoute exact path="/home" component={MainContainer} />
-                <AuthRoute path="/playlist/:id" component={MainContainer} />
-                <AuthRoute path="/artist/:id" component={MainContainer} />
-                <AuthRoute path="/album/:id" component={MainContainer} />
-            </Switch>
-        </div>
-    )
-)};
+    </div>
+);
 
 export default App;
