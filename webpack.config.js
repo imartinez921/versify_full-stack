731bdf78ad19cc8c8e83adb7a92e0b539edd4fce
Irene Martinez <imartinez921@gmail.com>
2023-07-21 14:58:15 -0400

    install dotenv and import into highest-level file

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-	context: __dirname,
-	entry: './frontend/versify.jsx',
-	output: {
-		path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [ '@babel/env', '@babel/react' ]
-					}
-				}
-			}
-		]
-	},
-	devtool: 'inline-source-map',
-	resolve: {
-		extensions: [ '.js', '.jsx', '*' ]
-	}
+    context: __dirname,
+    entry: "./frontend/versify.jsx",
+    output: {
+        path: path.resolve(__dirname, "app", "assets", "javascripts"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/env", "@babel/react"],
+                    },
+                },
+            },
+        ],
+    },
+    devtool: "inline-source-map",
+    resolve: {
+        extensions: [".js", ".jsx", "*"],
+        fallback: {
+            path: require.resolve("path-browserify"),
+            crypto: require.resolve("crypto-browserify"),
+        },
+    },
 };
